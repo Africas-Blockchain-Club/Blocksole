19f2eb3142d08b5734befaf4e7f831439f7fbf9a
Sihle <sshabangu061@gmail.com>
2024-07-11 22:32:08 +0200

    footer updated

--- packages/react-app/components/Footer.tsx
+++ packages/react-app/components/Footer.tsx
@@ -30,7 +30,7 @@ const navigation = [
   
   export default function Footer() {
     return (
-      <footer className="bg-gypsum mt-auto border-black border-t">
+      <footer className="bg-gypsum mt-auto border-black border-t fixed bottom-0 left-0 w-full">
         <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
           {/* <div className="flex justify-center space-x-6 md:order-2">
             {navigation.map((item) => (
