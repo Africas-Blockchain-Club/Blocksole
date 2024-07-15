2241e3d29a1d76fb2a280def9da57b12df10357d
Mpho Mofokeng <mpho@pop-os.localdomain>
2024-07-15 02:30:18 +0200

    Updated the getSneakerImages function

--- packages/react-app/store/getSneakerImages.tsx
+++ packages/react-app/store/getSneakerImages.tsx
@@ -4,7 +4,7 @@ import Sneaker from '@/types/sneaker';
 const getSneakerImages = (sneaker: Sneaker): string[] => {
   if (!sneaker.imageUrl || !sneaker.imageUrl[0]) return [];
 
-  const sneakerImages = sneaker.imageUrl[0].split("g,http").map((url, index) => {
+  const sneakerImages = sneaker.imageUrl[0].split(",http").map((url, index) => {
     return index === 0 ? url : "http" + url;
   });
 
