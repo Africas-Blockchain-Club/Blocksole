1edf81ae436bc5ee7bb742fdbe2169add2e8c324
Mpho Mofokeng <mpho@pop-os.localdomain>
2024-07-15 02:20:06 +0200

    Added a search button but has no functionality

--- packages/react-app/components/Layout.tsx
+++ packages/react-app/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import SearchBar from "@/pages/searchBar";
 
 interface Props {
     children: ReactNode;
@@ -10,7 +11,9 @@ const Layout: FC<Props> = ({ children }) => {
         <>
             <div className="bg-gypsum overflow-x-hidden flex flex-col min-h-screen">
                 <Header showSearch={false}/>
+                
                 <div className="py-16 sm:py-6 lg:py-8 max-w-full lg:max-w-7xl mx-auto space-y-8 px-4 ">
+                <SearchBar isVisible={false} />
                     {children}
                 </div>
                 <Footer />
