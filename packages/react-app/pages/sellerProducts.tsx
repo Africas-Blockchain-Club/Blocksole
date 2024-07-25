b6b7c819e00772b44975de6689f825e21876e801
Sihle (Bafo) Shabangu <82091092+Sihle-Shabangu@users.noreply.github.com>
2024-07-25 21:19:01 +0000

    User Experience Fixed and disabled button added

--- packages/react-app/pages/sellerProducts.tsx
+++ packages/react-app/pages/sellerProducts.tsx
@@ -17,6 +17,7 @@ const SellerProductsForm: React.FC = () => {
   const [size, setSize] = useState(0);
   const [stockAvailable, setStockAvailable] = useState(0);
   const [description, setDescription] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const { address } = useAccount();
   const router = useRouter();
@@ -26,6 +27,15 @@ const SellerProductsForm: React.FC = () => {
       setSeller(address);
     }
   }, [address]);
+
+  useEffect(() => {
+    // Check if all required fields are populated
+    if (brand && model && colorway && size && stockAvailable && description && price > 0 ) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [brand, model, colorway, size, stockAvailable, description, price, imageUrl]);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -95,18 +105,6 @@ const SellerProductsForm: React.FC = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-gray-600">Colorway</label>
-            <input 
-              type="text"
-              value={colorway}
-              onChange={(e) => setColorway(e.target.value)}
-              className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-orange-600" 
-              required
-            />
-          </div>
-          
-          
-          <div className="mb-4">
             <label className="block mb-2 text-sm font-medium text-gray-600">Model</label>
             <input 
               type="text"
@@ -117,11 +115,11 @@ const SellerProductsForm: React.FC = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-gray-600">Price (cUSDT)</label>
+            <label className="block mb-2 text-sm font-medium text-gray-600">Colorway</label>
             <input 
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(Number(e.target.value))}
+              type="text"
+              value={colorway}
+              onChange={(e) => setColorway(e.target.value)}
               className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-orange-600" 
               required
             />
@@ -136,16 +134,6 @@ const SellerProductsForm: React.FC = () => {
               required
             />
           </div>
-          {/* needs to move to update sneaker */}
-          {/* <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-gray-600">Is Available</label>
-            <input 
-              type="checkbox"
-              checked={isAvailable}
-              onChange={(e) => setIsAvailable(e.target.checked)}
-              className="form-checkbox" 
-            />
-          </div> */}
           <div className="mb-4">
             <label className="block mb-2 text-sm font-medium text-gray-600">Stock Available</label>
             <input 
@@ -167,14 +155,17 @@ const SellerProductsForm: React.FC = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-gray-600">Upload Sneaker Images</label>
-            {/* <input 
-              type="text"
-              value={imageUrl[0]}
-              onChange={(e) => setImageUrl([e.target.value])}
+            <label className="block mb-2 text-sm font-medium text-gray-600">Price (cUSDT)</label>
+            <input 
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(Number(e.target.value))}
               className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-orange-600" 
               required
-            /> */}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-600">Upload Sneaker Images</label>
             <div className='flex p-2'> 
               <div className="flex items-center justify-center w-full">
                   <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-15 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 ">
@@ -201,7 +192,7 @@ const SellerProductsForm: React.FC = () => {
                   </label>
               </div>
               <div className="flex items-center justify-center w-full">
-                  <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-15 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                  <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-15 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 m-1">
                       <div className="flex flex-col items-center justify-center pt-5 pb-6">
                           <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
@@ -211,23 +202,29 @@ const SellerProductsForm: React.FC = () => {
                       </div>
                       <input id="dropzone-file" type="file" className="hidden" />
                   </label>
-                  
-              </div> 
+              </div>
             </div>
-            <div className="flex flex-wrap mb-2 text-sm font-medium text-gray-600">
-              <p className='ml-12'>Top view</p>
-              <p className='mx-12'>Side view</p>
-              <p>Perspective view</p>
-            </div>
-
           </div>
-          <button type="submit" className="w-full bg-orange-600 text-white py-2 rounded-lg shadow-lg hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-blue-400">
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-600">Available</label>
+            <input 
+              type="checkbox"
+              checked={isAvailable}
+              onChange={(e) => setIsAvailable(e.target.checked)}
+              className="h-4 w-4 text-orange-600 focus:ring focus:ring-orange-600 border-gray-300 rounded" 
+            />
+          </div>
+          <button 
+            type="submit"
+            className="w-full py-2 px-4 bg-orange-600 hover:bg-orange-500 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring focus:ring-orange-500 disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={isButtonDisabled}
+          >
             Add Product
           </button>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default SellerProductsForm;
