e0f197ebae208e1443b7a6d41722cef12100d63f
Sihle (Bafo) Shabangu <82091092+Sihle-Shabangu@users.noreply.github.com>
2024-07-25 21:26:04 +0000

    Fixed the boy's forwardness

--- packages/react-app/pages/sellerProducts.tsx
+++ packages/react-app/pages/sellerProducts.tsx
@@ -173,7 +173,7 @@ const SellerProductsForm: React.FC = () => {
                           <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                           </svg>
-                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span></p>
                           <p className="text-xs text-gray-500 dark:text-gray-400">PNG or JPG(MAX. 800x400px)</p>
                       </div>
                       <input id="dropzone-file" type="file" className="hidden" />
@@ -185,7 +185,7 @@ const SellerProductsForm: React.FC = () => {
                           <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                           </svg>
-                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span></p>
                           <p className="text-xs text-gray-500 dark:text-gray-400">PNG or JPG(MAX. 800x400px)</p>
                       </div>
                       <input id="dropzone-file" type="file" className="hidden" />
@@ -197,15 +197,21 @@ const SellerProductsForm: React.FC = () => {
                           <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                           </svg>
-                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span></p>
                           <p className="text-xs text-gray-500 dark:text-gray-400">PNG or JPG(MAX. 800x400px)</p>
                       </div>
                       <input id="dropzone-file" type="file" className="hidden" />
                   </label>
               </div>
             </div>
+            <div className="flex flex-wrap mb-2 text-sm font-medium text-gray-600">
+              <p className='ml-12'>Top view</p>
+              <p className='mx-12'>Side view</p>
+              <p>Perspective view</p>
+            </div>
           </div>
-          <div className="mb-4">
+          {/* needs to move to update sneaker */}
+          {/* <div className="mb-4">
             <label className="block mb-2 text-sm font-medium text-gray-600">Available</label>
             <input 
               type="checkbox"
@@ -213,7 +219,7 @@ const SellerProductsForm: React.FC = () => {
               onChange={(e) => setIsAvailable(e.target.checked)}
               className="h-4 w-4 text-orange-600 focus:ring focus:ring-orange-600 border-gray-300 rounded" 
             />
-          </div>
+          </div> */}
           <button 
             type="submit"
             className="w-full py-2 px-4 bg-orange-600 hover:bg-orange-500 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring focus:ring-orange-500 disabled:opacity-50 disabled:cursor-not-allowed"
